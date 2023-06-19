6c1f4f4a099bdabb19735dc5cdf716f41fa2aeb7
richelbilderbeek <richel@richelbilderbeek.nl>
2023-06-19 09:17:38 +0200

    Add Rackham script

--- scripts/build_rackham.sh
+++ scripts/build_rackham.sh
@@ -2,7 +2,7 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 10:00
-#SBATCH --job-name gha_cmake_intel_cpp17
+#SBATCH --job-name gha_intel_cpp17
 #SBATCH --output build_rackham.log
 #SBATCH --qos=short
 #
@@ -27,44 +27,8 @@ fi
 module load intel/20.4
 module load cmake/3.22.2 
 
-# echo "Where is the icpc compiler?"
-# which icpc
-# -> "/sw/comp/intel/compilers_and_libraries_2020.4.304/linux/bin/intel64/icpc"
-
-# [richel@rackham1 bin]$ pwd
-# /sw/comp/intel/compilers_and_libraries_2020.4.304/linux/bin
-# [richel@rackham1 bin]$ ./icpx --version
-# Intel(R) oneAPI DPC++ Compiler 2021.1 (2020.8.0.0827)
-# Target: x86_64-unknown-linux-gnu
-# Thread model: posix
-# InstalledDir: /sw/comp/intel/compilers_and_libraries_2020.4.304/linux/bin
-
-# echo "Where is the icpx compiler?"
-# which icpx
-# -> cannot be found
-
-cmake -S . -B build \
-           -DCMAKE_BUILD_TYPE="RelWithDebInfo" \
-           -DCMAKE_INSTALL_PREFIX=install \
-           -DCMAKE_CXX_COMPILER=icpc \
-           -DCMAKE_C_COMPILER=icc \
-           -DIntelDPCPP_DIR="/sw/comp/intel/oneapi/compiler/latest/linux/cmake/SYCL" \
-           -DMKL_ROOT="/sw/comp/intel/oneapi/mkl/latest" \
-           -DTBB_ROOT="/sw/comp/intel/oneapi/tbb/latest"
-
-# Rackham location
+# Rackham location (GHA location: source /opt/intel/oneapi/setvars.sh)
 source /sw/comp/intel/oneapi/setvars.sh
-# GitHub Actions location
-# source /opt/intel/oneapi/setvars.sh
 
-cmake --build build
-./build/gha_cmake_intel_cpp17
-
-# Create the folder, even if it already exists
-#mkdir build
-#cd build
-#rm -f CMakeCache.txt
-#cmake ..
-#cmake --build . --config=Release --target all -j 10
-#cd ..
-
+icpx main.cpp -o gha_intel_cpp17
+./gha_intel_cpp17
