4ea8fb3efacb7158776d62ed6a37cc5cf32cd336
richelbilderbeek <richel@richelbilderbeek.nl>
2023-06-19 15:13:28 +0200

    Works locally

--- scripts/build_rackham.sh
+++ scripts/build_rackham.sh
@@ -30,28 +30,6 @@ fi
 date
 
 
-# Loading the needed modules
-module load intel-oneapi compiler
-
-# Show the module list, as requested by Pavlin
-module list
-
-# Rackham location (GHA location: source /opt/intel/oneapi/setvars.sh)
-# source /sw/comp/intel/oneapi/setvars.sh
-
+module load gcc/13.1.0 intel-oneapi compiler
 icpx main.cpp --verbose -o gha_intel_cpp17
-
-# All FAIL:
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/sw/comp/intel/oneapi/dpl/2022.1.0/linux/include/oneapi/dpl
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/sw/comp/gcc/13.1.0_rackham/include/c++/13.1.0
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/sw/comp/intel/oneapi/tbb/2021.9.0/include
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/sw/comp/intel/oneapi/compiler/2023.1.0/linux/bin-llvm/../compiler/include
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../include/c++/4.8.5
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../include/c++/4.8.5/x86_64-redhat-linux
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/usr/lib/gcc/x86_64-redhat-linux/4.8.5/../../../../include/c++/4.8.5/backward
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/sw/comp/intel/oneapi/compiler/2023.1.0/linux/lib/clang/16/include
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/usr/local/include
-# icpx main.cpp --verbose -o gha_intel_cpp17 -I/usr/include
-
-
 ./gha_intel_cpp17
